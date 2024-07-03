3fa62f4b56d31d7de586e04bf807834969d23346
Paul Wassermann <paul9499@gmail.com>
2024-07-03 14:36:41 +0200

    format(wip): ruff configuration

--- src/tokenizer/simple_tokenizer.py
+++ src/tokenizer/simple_tokenizer.py
@@ -3,20 +3,24 @@ from .tokenizer import Tokenizer
 
 class SimpleTokenizer(Tokenizer):
     """
-    The `SimpleTokenizer` class implements the interface defined in :class:`Tokenizer`.
+    The `SimpleTokenizer` class implements the interface defined in 
+    :class:`Tokenizer`.
 
-    The :method:`train` method implements a simple Byte Pair Encoding (BPE) algorithm, with no consideration whatsoever for wasteful / useless merges. This tokenizer does not support special token handling.
+    The :method:`train` method implements a simple Byte Pair Encoding (BPE) 
+    algorithm, with no consideration whatsoever for wasteful / useless merges. 
+    This tokenizer does not support special token handling.
 
     Attributes
     ----------
     vocab : dict (int -> bytes)
-        A mapping from the token ids to the actual bytes they encode. The bytes can be 
-        converted to text using UTF-8 decoding. However, there is no guarantee that the
-        bytes are actually valid UTF-8.
+        A mapping from the token ids to the actual bytes they encode. The bytes 
+        can be converted to text using UTF-8 decoding. However, there is no 
+        guarantee that the bytes are actually valid UTF-8.
 
-    merges: dict ((int, int) -> str)  
+    merges: dict ((int, int) -> str)
         A mapping from pairs of token ids to the 'merged' token ids.
     """
+
     def decode(self, token_ids: list[int]) -> str:
         return b"".join(
             self.vocab[token_id] for token_id in token_ids
@@ -28,5 +32,4 @@ class SimpleTokenizer(Tokenizer):
         for token_pair, pair_id in self.merges:
             token_ids = list(self._merge(token_ids, token_pair, pair_id))
 
-    def train(self):
-        ...
+    def train(self): ...
--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -6,63 +6,65 @@ from typing import Generator, Self, Sequence
 class Tokenizer(ABC):
     """
     Base class defining an interface for concrete tokenizers.
-    
+
     Attributes
     ----------
     vocab : dict (int -> bytes)
-        A mapping from the token ids to the bytes they encode. The bytes can be converted to text using UTF-8 decoding. However, there is no guarantee that the bytes are actually valid UTF-8.
+        A mapping from the token ids to the bytes they encode. The bytes 
+        can be converted to text using UTF-8 decoding. However, there is 
+        no guarantee that the bytes are actually valid UTF-8.
 
-    merges: dict ((int, int) -> str)  
+    merges: dict ((int, int) -> str)
         A mapping from pairs of token ids to the 'merged' token ids.
     """
+
     def __init__(self):
         self.vocab: dict[int, bytes] = {
             bytes(chr(i), encoding="utf-8") for i in range(256)
-            } 
+        }
         self.merges: dict[tuple[int, int], int] = {}
-    
+
     @abstractmethod
     def decode(self, token_ids: list[int]) -> str:
         """
-        Decodes a list of token ids to a human-readable text. Decoding is performed assuming UTF-8. In case of an invalid bytes sequence, said sequence is simply replaced with a special character.
+        Decodes a list of token ids to a human-readable text. Decoding is 
+        performed assuming UTF-8. In case of an invalid bytes sequence, said 
+        sequence is simply replaced with a special character.
 
         Parameters
         ----------
         token_ids: list of int
-            A list of integer tokens, with values greater than zero and inferior than the vocabulary size.
+            A list of integer tokens, with values greater than zero and 
+            inferior than the vocabulary size.
 
         Returns
         -------
         str
             The decoded string.
         """
-    
+
     @abstractmethod
-    def encode(self, text: str) -> list[int]:
-        ...
-    
+    def encode(self, text: str) -> list[int]: ...
+
     @abstractmethod
-    @classmethod
-    def load(self, path: str | PathLike) -> Self:
-        ...
+    @staticmethod
+    def load(path: str | PathLike) -> Self: ...
 
     def _merge(
-            self, token_ids: Sequence[int], id_pair: tuple[int, int], new_id: int
-            ) -> Generator[int, None, None]:
+        self, token_ids: Sequence[int], id_pair: tuple[int, int], new_id: int
+    ) -> Generator[int, None, None]:
         index = 0
         while index < len(token_ids) - 1:
             if (token_ids[index], token_ids[index + 1]) == id_pair:
                 yield new_id
             else:
                 yield token_ids[index]
-        
+
         if index == len(token_ids) - 1:
             yield token_ids[index]
 
     @abstractmethod
-    def save(self, path: str | PathLike) -> None:
-        ...
+    def save(self, path: str | PathLike) -> None: ...
 
     @abstractmethod
-    def train(self, training_data: str, num_merges: int) -> None:
-        ...
+    def train(self, training_data: str, num_merges: int) -> None: ...
